eb7e9884646ecc907b60825ad27c718e7cc20597
PeteJames21 <peetjames21@gmail.com>
2023-09-25 10:21:23 +0300

    Implement the selection sort algorithm

---
+++ 2-selection_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sort an array in ascending order using selection sort
+ * @array: pointer to array to be sorted
+ * @size: the length of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min_index;
+	int min, tmp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_index = i;
+		min = array[i];
+		/* Look for the min value in the sublist to the right */
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < min)
+			{
+				min = array[j];
+				min_index = j;
+			}
+		}
+		if (min_index != i)
+		{
+			/* Swap array[i] with the min value */
+			tmp = array[i];
+			array[i] = array[min_index];
+			array[min_index]  = tmp;
+			print_array(array, size);
+		}
+	}
+}
--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Defines a function that sorts an array of integers in ascending order using the 
 
 ## 0-O
 Lists the best, average, and worst case big O notation of the Bubble sort algorithm, respectively.
+
+## 2-selection_sort.c
+Defines a function that sorts an array of integers in ascending order using the Selection sort algorithm. The array is printed each time two elements are swapped.
+
+## 2-O
+Lists the best, average, and worst case big O notation of the Selection sort algorithm, respectively.
